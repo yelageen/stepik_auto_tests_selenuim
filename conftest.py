c3e066dd398114d25790bd63b0e67452323dd15f
yelageen <yelageen@gmail.com>
2022-11-16 14:13:59 +0300

    Changed conftest.py for processing '--browser_name' command line key

--- conftest.py
+++ conftest.py
@@ -2,10 +2,21 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
+def pytest_addoption(parser):
+    parser.addoption('--browser_name', action='store', default=None, help="Choose browser: chrome or edge")
+
 @pytest.fixture(scope="function")
-def browser():
-    print("\nstart browser for test..")
-    browser = webdriver.Chrome()
+def browser(request):
+    browser_name = request.config.getoption('browser_name')
+    if browser_name == "chrome":
+        browser = webdriver.Chrome()
+    elif browser_name == "edge":
+        browser = webdriver.Edge()
+    else:
+        raise pytest.UsageError('--browser_name should be chrome or edge')
+    print(f'\nStart [{browser_name}] browser for test...')
+
     yield browser
-    print("\nquit browser..")
+
+    print(f'\nQuit [{browser_name}] browser...')
     browser.quit()
